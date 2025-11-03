fd1de5442d8ce5cf16f128a0c9c164727f2e7d3b
Jose Mateos <zs24022199@estudiantes.uv.mx>
2025-11-03 15:42:29 -0600

    ultimo commit

--- src/App.java
+++ src/App.java
@@ -3,16 +3,16 @@ import java.util.ArrayList;
 public class App {
     public static void main(String[] args) throws Exception {
 
-        Cuenta cuenta1 = new Cuenta(1, 100, 4.5, null);
-        Cuenta cuenta2 = new Cuenta(2, 100, 4.5, null);
-        Cuenta cuenta3 = new Cuenta(3, 100, 4.5, null);
-        Cuenta cuenta4 = new Cuenta(4, 100, 4.5, null);
-        Cuenta cuenta5 = new Cuenta(5, 100, 4.5, null);
-        Cuenta cuenta6 = new Cuenta(6, 100, 4.5, null);
-        Cuenta cuenta7 = new Cuenta(7, 100, 4.5, null);
-        Cuenta cuenta8 = new Cuenta(8, 100, 4.5, null);
-        Cuenta cuenta9 = new Cuenta(9, 100, 4.5, null);
-        Cuenta cuenta10 = new Cuenta(10, 100, 4.5, null);
+        Cuenta cuenta1 = new Cuenta(0, 100, 4.5, null);
+        Cuenta cuenta2 = new Cuenta(1, 100, 4.5, null);
+        Cuenta cuenta3 = new Cuenta(2, 100, 4.5, null);
+        Cuenta cuenta4 = new Cuenta(3, 100, 4.5, null);
+        Cuenta cuenta5 = new Cuenta(4, 100, 4.5, null);
+        Cuenta cuenta6 = new Cuenta(5, 100, 4.5, null);
+        Cuenta cuenta7 = new Cuenta(6, 100, 4.5, null);
+        Cuenta cuenta8 = new Cuenta(7, 100, 4.5, null);
+        Cuenta cuenta9 = new Cuenta(8, 100, 4.5, null);
+        Cuenta cuenta10 = new Cuenta(9, 100, 4.5, null);
 
         ArrayList<Cuenta> cuentas = new ArrayList<>();
 
@@ -27,5 +27,13 @@ public class App {
         cuentas.add(cuenta9);
         cuentas.add(cuenta10);
 
+        switch (cuenta1.getId()) {
+            case 0:
+
+                break;
+
+            default:
+                break;
+        }
     }
 }
