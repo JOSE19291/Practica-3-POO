0da64bd3f5efea62caf3c8c075cd904834ff3637
Jose Mateos <zs24022199@estudiantes.uv.mx>
2025-11-03 15:32:43 -0600

    segundo commit

--- src/App.java
+++ src/App.java
@@ -1,22 +1,16 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        /*
-         * Cuenta cuenta1 = new Cuenta(1, 100, 4.5, null);
-         * System.out.println("Tu saldo ahora por retirar 2500 es: " +
-         * cuenta1.withdraw(2500));
-         * System.out.println("Tu saldo ahora por depositar 3000 es: " +
-         * cuenta1.deposit(3000));
-         */
 
-        ArrayList<Cuenta> cuentas = new ArrayList<Cuenta>();
+        Cuenta cuenta1 = new Cuenta(1, 100, 4.5, null);
+        System.out.println("Tu saldo ahora por retirar 2500 es: " +
+                cuenta1.withdraw(2500));
+        System.out.println("Tu saldo ahora por depositar 3000 es: " +
+                cuenta1.deposit(3000));
 
-        cuentas.add(0, null);
-        cuentas.add(1, null);
+        ArrayList<Cuenta> cuentas = new ArrayList<>();
 
-        for (Cuenta cuenta : cuentas) {
-            cuenta.getId();
-        }
     }
 }
